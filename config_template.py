2c725afc47094500aeedc9257d9ee5ae25c97fe9
Sampath Eaty <s.eaty6@gmail.com>
2022-03-09 13:36:56 -0500

    Finished funciton to process track_set and artist_set

---
+++ config_template.py
@@ -0,0 +1,4 @@
+last_fm_apikey = 'last_fm_apikey'
+last_fm_secret = 'last_fm_secret'
+last_fm_username = 'last_fm_username'
+last_fm_password = 'last_fm_password'
--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from config import last_fm_apikey, last_fm_secret, last_fm_password, last_fm_use
 last_fm_passwordMD5 = pylast.md5(last_fm_password)
 
 last_fm_object = pylast.LastFMNetwork(api_key=last_fm_apikey, api_secret=last_fm_secret, username=last_fm_username, password_hash=last_fm_passwordMD5)
-track_set = set()
+
+
+test_list = []
 
 def get_history(number_of_songs, username):
     #if number_of_songs = 0, then download all - minimum, 500
+    track_set = set()
     user_object = last_fm_object.get_user(username)
 
     if number_of_songs == 0:
-        all_songs = True 
-        limit = 'None'
+        limit = None
         loop_counter = 0
     else:
         limit = number_of_songs%500 + 1
         loop_counter = number_of_songs//500
     
-    listening_history = user_object.get_recent_tracks(limit=limit, cacheable=True)
+    listening_history = user_object.get_recent_tracks(limit=limit, cacheable=True, stream=True)
     for PlayedTrack in listening_history:
         last_retrieved_track_timestamp = ''
         last_retrieved_track_timestamp = str(PlayedTrack[3])
@@ -31,7 +33,7 @@ def get_history(number_of_songs, username):
             pass
     
     while 0 < loop_counter:
-        listening_history = user_object.get_recent_tracks(limit=500, cacheable=True, time_to=last_retrieved_track_timestamp)
+        listening_history = user_object.get_recent_tracks(limit=500, cacheable=True, time_to=last_retrieved_track_timestamp, stream=True)
         for PlayedTrack in listening_history:
             last_retrieved_track_timestamp = ''
             last_retrieved_track_timestamp = str(PlayedTrack[3])
@@ -43,27 +45,64 @@ def get_history(number_of_songs, username):
             except UnicodeEncodeError:
                 pass
         loop_counter = loop_counter - 1
-            
+    return track_set         
 
         
 
 
 
     print(track_set)
+    print(len(track_set))
     print(last_retrieved_track_timestamp)
-
+    
+def get_artist(artist_name, num_songs):
+    artist_set = set()
+    artist_object = last_fm_object.get_artist(artist_name)
+    things = artist_object.get_top_tracks(limit=num_songs, stream=True)
+    for element in things:
+        temp_str = (element[0])
+        try:
+           # prints song name just pulled 
+            print(temp_str) 
+            temp_str = str(temp_str)
+            artist_set.add(temp_str)
+        except UnicodeEncodeError:
+            pass
+    for element in artist_set:
+        artist_name_corrected = element.split(' - ')[0].strip()
+        break
+    return artist_set, artist_name_corrected
     
 
-
-
-    
-def get_artist():
-    return(input("Name an Artist\n"))
+def process_set(input_set):
+    processed_set = set()
+    for element in input_set:
+        remove_chars = ['(', '[', 'ft.', 'feat.']
+        for phrase in remove_chars:
+            if element.find(phrase) != -1:
+                element = element[:element.find(phrase)].strip()
+        if element.count(' - ') == 1:
+            processed_set.add(element)
+        elif element.count(' - ') > 1:
+            first_found_location = (element.find(' - '))
+            element = element[:(element.find(' - ',first_found_location+1,-1))]
+            processed_set.add(element)
+    return processed_set
 
 def main():
-    #print("Hello world")
-    get_history(850, 'seaty6')
-    #print(get_artist())
+    print("Hello world")
+    track_set = get_history(10, 'seaty6')
+    print("Length of track Set, Unprocessed: " + str(len(track_set)))
+    track_set_processed = process_set(track_set)
+    print("Length of track Set, processed: " + str(len(track_set_processed)))
+
+    artist_set, artist_name_corrected = get_artist('Kanye', 10)
+    print(artist_name_corrected)
+    print("Length of artist Set, Unprocessed: " + str(len(artist_set)))
+    artist_set_processed = process_set(artist_set)
+    print("Length of artist Set, processed: "    + str(len(artist_set_processed)))
+
+
 
 if __name__ == "__main__":
     main()
